65997b7ab98f32742ba8e417e40617703f548199
wawhal <rishichandra.wawhal@gmail.com>
2019-04-13 13:19:08 +0530

    make raw ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return "Hello";
+    return (
+      <div className="mainWrapper">
+        <Textbox />
+        <Authors />
+      </div>
+    )
   }
+}
+
+const Textbox = () => {
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+  const handleOnClick = () => {
+    alert('Clicked')
+  }
+  return (
+    <div className="textboxWrapper"> 
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        className="textbox"
+      />
+      <button
+        onClick={handleOnClick}
+        type="submit"
+        className="addButton"
+      >
+        Add
+      </button>
+    </div>
+  )
+}
+
+const Authors = () => {
+  const data = {
+    authors: [
+      {
+        id: 1,
+        name: 'Shahidh'
+      },
+      {
+        id: 2,
+        name: 'Rishi'
+      },
+      {
+        id: 3,
+        name: 'Suri'
+      },
+      {
+        id: 4,
+        name: 'Shark'
+      },
+      {
+        id: 5,
+        name: 'Tanmai'
+      },
+    ]
+  };
+  return (
+    <div className="authorsWrapper"> 
+      {
+        data.authors.map(a => {
+          return (
+            <div className="authorElement">
+              {a.name} 
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
